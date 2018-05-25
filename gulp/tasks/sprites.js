de81b8578af660b6b42278e760d0ead88fd6d4ed
Jacek Dziurdzikowski <jacek.dziurdzikowski@gmail.com>
2018-05-25 22:54:22 +0800

    Implemented icons on webpage to use sprites and improves automation process of preparing sprites

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
-    svgSprite = require('gulp-svg-sprite');
+    svgSprite = require('gulp-svg-sprite'),
+    gulpRename = require('gulp-rename'),
+    sequence = require('run-sequence');
 
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
             render: {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -14,7 +17,24 @@ var config = {
 }
 
 gulp.task('createSprite', function () {
-    gulp.src('./app/assets/images/icons/**/*svg')
+    return gulp.src('./app/assets/images/icons/**/*svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySvg', function(){
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+})
+
+gulp.task('copyCss', function(){
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(gulpRename({
+            prefix: '_'
+        }))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', function () {
+    sequence('createSprite', 'copySvg','copyCss');
 })
